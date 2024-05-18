518ac150bf9d47d881e1896d7c09b0df90ab4831
ssdasgupta <ssdasgupta@umass.edu>
2024-05-18 09:40:58 -0400

    added abletion testing code

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ from data_loaders.data_processing import (
     DataProcessing,
     MovieLensDataProcessing, 
     JointDataProcessing,
-    JointDataProcessingWithAttributesLeaked
+    JointDataProcessingWithAttributesLeaked,
+    JointDataProcessingWithUsersLeaked,
+    JointDataProcessingWithAllLeaked
 )
 
 torch.backends.cudnn.deterministic = True
@@ -67,7 +69,8 @@ def main():
     parser.add_argument('--dataset_type',
                         type=str,
                         default='joint',
-                        choices=['synthetic', 'movielens', 'user-item', 'attribute-item', 'joint', 'joint-attribute'],
+                        choices=['synthetic', 'movielens', 'user-item', 'attribute-item',
+                                 'joint', 'joint-attribute', 'joint-user', 'joint-all'],
                         help='model type')
     parser.add_argument('--data_dir',
                         type=str,
@@ -157,6 +160,24 @@ def main():
         n_item_attrs = dataset.n_attributes
     elif args.dataset_type == 'joint-attribute':
         dataset = JointDataProcessingWithAttributesLeaked(data_dir=args.data_dir,
+                                        dataset_type=args.dataset_type,
+                                        batch_size=args.batch_size,
+                                        eval_batch_size=args.eval_batch_size)
+        n_users = dataset.n_users
+        n_items = dataset.n_movies
+        n_user_attrs = 0
+        n_item_attrs = dataset.n_attributes
+    elif args.dataset_type == 'joint-user':
+        dataset = JointDataProcessingWithUsersLeaked(data_dir=args.data_dir,
+                                        dataset_type=args.dataset_type,
+                                        batch_size=args.batch_size,
+                                        eval_batch_size=args.eval_batch_size)
+        n_users = dataset.n_users
+        n_items = dataset.n_movies
+        n_user_attrs = 0
+        n_item_attrs = dataset.n_attributes
+    elif args.dataset_type == 'joint-all':
+        dataset = JointDataProcessingWithAllLeaked(data_dir=args.data_dir,
                                         dataset_type=args.dataset_type,
                                         batch_size=args.batch_size,
                                         eval_batch_size=args.eval_batch_size)
